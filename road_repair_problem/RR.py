2416ac75c36a3094b8ecbe56e9b0c9ecee8fcc2c
moussa <ahmedmoussa.dev@gmail.com>
2025-02-15 10:27:30 +0200

    implemented worker "awareness of nearby roads"

--- road_repair_problem/RR.py
+++ road_repair_problem/RR.py
@@ -2,17 +2,17 @@ import random
 #TODO: import icecream?
 
 # Variables
-# road_size = [1, 2, 3, 4, 5, 6, 7]
-# fixing_speed = [1, 2, 3, 4, 5, 6, 7]
-# area_of_effect = [1, 2, 3, 4, 5, 6, 7]
-# distance_from_road = [1, 2, 3, 4, 5,6, 7]
-# number_of_free_workers = [1, 2, 3, 4, 5, 6, 7] #TODO: make variables actually useful
+road_size = [1, 2, 3, 4, 5, 6, 7]
+fixing_speed = [1, 2, 3, 4, 5, 6, 7]
+area_of_effect = [1, 2, 3, 4, 5, 6, 7]
+distance_from_road = [1, 2, 3, 4, 5,6, 7]
+number_of_free_workers = [1, 2, 3, 4, 5, 6, 7] #TODO: make variables actually useful
 
-road_size = [1]
-fixing_speed = [1]
-area_of_effect = [1]
-distance_from_road = [1]
-number_of_free_workers = [1] #TODO: make variables actually useful
+# road_size = [1]
+# fixing_speed = [1]
+# area_of_effect = [1]
+# distance_from_road = [1]
+# number_of_free_workers = [3] # for testing purposes
 
 
 def random_choice(list_of_stuff):
@@ -55,14 +55,58 @@ class city:
             self.neighboring_roads.append(choice)
             self.roads_copy.remove(choice)
             self.number_of_roads_copy -= 1
-        
+            
     
     
     def print_stats(self):
         print(f"City: {self.name}")
         print(f"number_of_roads: {self.number_of_roads}")
         print(f"neighboring_roads: {self.neighboring_roads}")
+    
+    def __str__(self):
+        return self.name  # Returns the city name when printed
+
+    def __repr__(self):
+        return f"City({self.name})"  # For debugging
         
+        
+        
+
+class worker:
+    def __init__(self, name, company_pos):
+        self.name = name
+        self.fixing_speed = random_choice(fixing_speed)
+        self.curr_pos = company_pos
+
+    def print_stats(self):
+        print(f"worker: {self.name}")
+        print(f"Fixing Speed: {self.fixing_speed}")
+        print(f"Current position: {self.curr_pos}")
+        
+    def fix_road(self):
+        # check the nearby roads
+        print(f"nearby_roads: {self.curr_pos.neighboring_roads}")
+        
+    def move(self):
+        
+        # check the nearby roads
+        print(f"nearby_roads: {self.curr_pos.neighboring_roads}")
+        
+        self.fix_road(self)
+        
+        available_cities = []
+        
+        for city in cities:
+            for road in self.curr_pos.neighboring_roads:
+                if road in city.neighboring_roads and self.curr_pos != city:
+                    available_cities.append(city)
+        available_cities = list(set(available_cities))  
+        print(f"available cities to go to are: {available_cities}")
+        
+        
+                
+    
+    
         
 
 # Company class
@@ -74,37 +118,27 @@ class company:
         self.number_of_free_workers = random_choice(number_of_free_workers)
         self.city_of_origin = random_choice(cities)
         
-        self.workers = setup_worker(self.name)
+        self.num_workers = random_choice(number_of_free_workers)
+        self.workers = [worker(chr(65 + i) + f": {name}", self.city_of_origin) for i in range(self.num_workers)]
+        
         
         
         # if road is broken then fix else go to next city and find new roads to fix (breadth search first bsf)
-    def check_roads():
-        pass
+    def send_workers_out(self):
+        # self.workers[0].move()
+        for worker in self.workers:
+            worker.move()
   
     def print_stats(self):
         print(f"Company: {self.name}")
         print(f"Area of Effect: {self.area_of_effect}")
         print(f"Distance from Road: {self.distance_from_road}") #TODO: FIX
         print(f"Number of Free Workers: {self.number_of_free_workers}")
-        print(f"City of origin: {self.city_of_origin.name}")
+        print(f"City of origin: {self.city_of_origin}")
         print(f"workers: {[worker.name for worker in self.workers]}")
         print("-" * 30)
         
-class worker:
-    def __init__(self, name):
-        self.name = name
-        self.fixing_speed = random_choice(fixing_speed)
-        # self.curr_pos = #TODO: add the position of the company origin then move it with the move() function
-
-    def print_stats(self):
-        print(f"worker: {self.name}")
-        print(f"Fixing Speed: {self.fixing_speed}")
         
-    def move(self):
-        pass
-    
-    def fix_road(self):
-        pass
         
         
 # Game setup
@@ -120,13 +154,10 @@ def setup_cities():
 
 def setup_companies():
     num_companies = int(input('input the number of companies: '))
+    # num_companies = int(input('input the number range of workers: '))
     companies = [company(chr(65 + i) + f"({i})") for i in range(num_companies)]  #TODO: add same company different branch later
     return companies
 
-def setup_worker(name):
-    num_workers = random_choice(number_of_free_workers)
-    workers = [worker(chr(65 + i) + f": {name}") for i in range(num_workers)] 
-    return workers
     
     
 
@@ -157,5 +188,9 @@ if __name__ == "__main__":
     for comp in companies:
         for worker in comp.workers:
             worker.print_stats()
+            
+    companies[0].send_workers_out()
+    
+    # loop on all roads and if all roads are fixed print the data
             
 #TODO: next add worker functionality
